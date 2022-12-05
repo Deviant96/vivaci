ddd21ad85ddc35c736ae67c722a47917dbeace89
Miretazam Ciptaprima <deviant@Miretazams-MacBook-Air.local>
2022-12-05 20:19:00 +0700

    About and about team members responsive layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,22 +3,49 @@
 {{-- Image zooming component --}}
 @php($imageZoom = true)
 
+@section('style')
+    <style>
+        .founder-img {
+            object-fit: contain;
+            max-height: 100%;
+            width: 100%;
+        }
+
+        @media (max-width: 576px) {
+            .founder-img {
+                max-height: 300px;
+            }
+
+            .team-title {
+                font-size: 1.2em;
+            }
+        }
+
+        .team-img {
+            /* width:100%;
+            min-height: 400px;
+            max-height:400px;
+            object-fit:cover; */
+        }
+    </style>
+@endsection
+
 @section('content')
     <section>
         <div class="container top-margin">
             <div class="row">
                 <div class="col-12">
 
-                    <div style="width: 100%; height: 600px;">
+                    <div class="w-100 h-auto">
                         <img style="max-height: 100%; width: 100%;object-fit: cover;"
                             src="{{ asset('/img/teams/allteam.jpg') }}" alt="Vivaci Living Teams" />
                     </div>
 
-                    <div class="row d-flex justify-content-center py-5 mt-5">
-                        <div class="col-6">
+                    <div class="row d-flex justify-content-center py-3 py-sm-5 mt-0 ms-sm-5">
+                        <div class="col-12 col-md-6">
                             <h2 class="font-big-one">About Us</h2>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12 col-md-6">
                             <p class="font-para-one">We are a team with talented designers
                                 and great craftsmanship. We want to bring a thoughtful design to the space, not just
                                 aesthetically pleasing, but also functional. Client is our priority. We want to understand
@@ -39,20 +66,20 @@
             <div class="row">
                 <div class="col-6">
                     <div class="d-flex flex-column justify-content-center align-items-center">
-                        <div class="img-container w-100">
-                            <img src="{{ asset('/img/teams/founder.jpg') }}" alt="Founder">
+                        <div class="img-container w-100 text-center">
+                            <img src="{{ asset('/img/teams/founder.jpg') }}" alt="Founder" class="founder-img">
                         </div>
-                        <h3 class="text-center mt-3">Andrew Thomas Kading</h3>
-                        <p class="text-center">Founder</p>
+                        <h3 class="text-center mt-3 team-title">Andrew Thomas Kading</h3>
+                        <p class="fst-italic">Founder</p>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="d-flex flex-column justify-content-center align-items-center">
                         <div class="img-container w-100">
-                            <img src="{{ asset('/img/teams/cofounder.jpg') }}" alt="Founder">
+                            <img src="{{ asset('/img/teams/cofounder.jpg') }}" alt="Founder" class="founder-img">
                         </div>
-                        <h3 class="text-center mt-3">Lia Yuliana</h3>
-                        <p class="text-center">Co-Founder</p>
+                        <h3 class="text-center mt-3 team-title">Lia Yuliana</h3>
+                        <p class="fst-italic">Co-Founder</p>
                     </div>
                 </div>
             </div>
@@ -67,11 +94,12 @@
                         <div class="d-flex flex-column justify-content-center align-content-center">
                             <div class="img-container">
                                 <img src="{{ Voyager::image($item->image) }}" alt="Design Team"
-                                style="width:100%;min-height: 400px;max-height:400px;object-fit:cover;">
+                                    class="founder-img team-img">
                             </div>
-                            <h3 class="text-center mt-3">{{ $item->name }} Team</h3>
-                            <p class="text-center">{!! $item->description !!}</p>
-                            <div class="text-center"><a class="primary-link" href="{{ url()->current() . "/" . $item->slug }}">View members</a></div>
+                            <h3 class="text-center mt-3 team-title">{{ $item->name }} Team</h3>
+                            <p class="text-center fst-italic">{!! $item->description !!}</p>
+                            <div class="text-center"><a class="primary-link"
+                                    href="{{ url()->current() . '/' . $item->slug }}">View members</a></div>
                         </div>
                     </div>
                 @endforeach
--- resources/views/aboutteam.blade.php
+++ resources/views/aboutteam.blade.php
@@ -2,24 +2,65 @@
 
 @php($imageZoom = true)
 
+@section('style')
+<style>
+    .about-member h3 {font-size: 1.2rem;}
+    .member-img-container {
+        max-height: 350px;
+    }
+    .member-img {
+        width: 100%;
+    }
+    @media (min-width:1200px) {
+        .member-img-container {
+            max-height: 600px;
+        }
+    }
+    @media (max-width:1200px) {
+        .member-img-container {
+            max-height: 490px;
+        }
+    }
+    @media (max-width:992px) {
+        .member-img-container {
+            max-height: 350px;
+        }
+    }
+    @media (max-width:768px) {
+        .member-img-container {
+            max-height: 247px;
+        }
+    }
+    @media (max-width:576px) {
+        .member-img-container {
+            max-height: 350px;
+        }
+    }
+</style>
+@endsection
+
 @section('content')
     <section>
 
         <div class="container-fluid">
 
             <div class="row">
-                <div class="col-6 d-flex flex-column justify-content-center px-5">
-                    <div class="ms-5">
+                <div class="col-12 col-md-6 order-2 order-md-1 d-flex flex-column justify-content-center px-0 px-md-5 mt-5 mt-md-0">
+                    <div class="ms-0 ms-md-5 px-4 px-md-0">
                         <h2 class="fw-bold font-big-one">Explore our <span class="text-lowercase">{{ $details->name }}</span> team at
                             {{ setting('site.title') }}</h2>
                         <div class="mt-3">{!! $details->description !!}</div>
                     </div>
                 </div>
-                <div class="col-6" style="padding:0">
-                    <div class="text-end"
+                <div class="col-12 col-md-6 order-1 order-md-2" style="padding:0">
+                    <div class="text-end d-none d-md-block"
                         style="background: url({{ Voyager::image($details->image) }}) no-repeat center;background-size:cover;min-height: 100vh;">
 
                     </div>
+                    <div class="img-container d-block d-md-none">
+                        <img class="w-100" src="{{ Voyager::image($details->image) }}" alt="Photo of the team">
+                    </div>
+
                 </div>
             </div>
 
@@ -29,7 +70,7 @@
 
     <section>
 
-        <div class="container py-5">
+        <div class="container py-5 about-member">
 
             <div class="row">
                 <div class="col-12">
@@ -40,12 +81,12 @@
                             </div>
                         @else
                             @foreach ($em as $e)
-                                <div class="col-4">
-                                    <div class="img-container" style="max-height: 600px;">
-                                        <img class="h-100 w-100" src="{{ Voyager::image($e->photo) }}" />
+                                <div class="col-6 col-sm-4">
+                                    <div class="img-container member-img-container">
+                                        <img class="member-img" src="{{ Voyager::image($e->photo) }}" />
                                     </div>
                                     <h3 class="mt-3">{{ $e->name }}</h3>
-                                    <p class="mt-2">{{ $e->title }}</p>
+                                    <p class="mt-2 fst-italic">{{ $e->title }}</p>
                                 </div>
                             @endforeach
                         @endif

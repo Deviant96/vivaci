978724c0aa865af54ce8291b8990c729291956fd
Miretazam Ciptaprima <deviant@Miretazams-MacBook-Air.local>
2022-12-07 12:31:29 +0700

    Banner layout revamp, top navigation menu fixes, added alt and lazy loading to images

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -4,78 +4,85 @@
 @php($imageZoom = true)
 
 @section('style')
-<style>
-    .banner-img-container {
-        height: 300px;
-    }
-    @media (max-width:576px) {
+    <style>
+        .navbar:not(.nav-active) .navbar-nav > .nav-item > .nav-link {
+            color: #ffffff;
+        }
         .banner-img-container {
-            height: 170px;
-            background-position: bottom;
+            height: 350px;
+            overflow: hidden;
+            position: relative;
         }
-    }
-    .banner-img {
-            background-size:cover;
-            background-position: center;
-            background-repeat:no-repeat;
-            background-attachment: fixed;
-        }
-    @media (max-width:576px) {
-        .banner-img {
-            background-position: bottom;
-        }
-    }
 
-    .project-img-container {
-        height: 240px;
-    }
-    @media (max-width:1200px) {
-        .project-img-container {
-            height: 150px;
+        @media (max-width:576px) {
+            .banner-img-container {
+                height: 170px;
+                background-position: bottom;
+            }
         }
-    }
-    @media (max-width:576px) {
-        .project-img-container {
-            height: 120px;
+
+        .banner-img {
+            /* background-size: cover;
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-attachment: fixed; */
+            height: 100%;
+            position: absolute;
+            width: 100%;
+            z-index: -1;
         }
-    }
-    .project-img {
-        object-fit: cover;
-    }
-</style>
+
+        @media (max-width:576px) {
+            .banner-img {
+                background-position: bottom;
+            }
+        }
+
+        .project-img-container {
+            height: 240px;
+        }
+
+        @media (max-width:1200px) {
+            .project-img-container {
+                height: 150px;
+            }
+        }
+
+        @media (max-width:576px) {
+            .project-img-container {
+                height: 120px;
+            }
+        }
+
+        .project-img {
+            object-fit: cover;
+        }
+    </style>
 @endsection
 
 @section('content')
-    <section>
-        <div class="container top-margin">
-
-            <div class="row py-3 mt-3">
-                <div class="col-12 col-sm-6 d-sm-flex flex-column justify-content-end">
-                    <h2 class="fw-bold">{{ $project->title }}</h2>
-                    <div class="mb-0">{!! $project->desc !!}</div>
-                </div>
-                <div class="col-12 col-sm-6 d-sm-flex justify-content-end align-items-end">
-                    <div class="text-sm-end">
-                        <div>
-                            <h4 class="fw-bold">{{ $project->location }}</h4>
-                        </div>
-                        <div>
-                            <p class="text-muted">{{ $project->date }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </section>
-
     <section>
         <div class="container-fluid">
 
             <div class="row">
                 <div class="col-12 p-0 banner-img-container">
-                    <div class="h-100 banner-img"
-                        style="background-image: url({{ Voyager::image($project->image) }});">
+                    {{-- style="background-image: url({{ Voyager::image($project->image) }});filter:brightness(0.4);"> --}}
+                    <div class="banner-img">
+                        <img src="{{ Voyager::image($project->image) }}" alt="" class="position-absolute w-100"
+                            style="top: 50%;left:0;transform:translateY(-50%);filter:brightness(0.4);" />
+                    </div>
+
+                    <div class="h-100 d-flex flex-column justify-content-center align-items-center text-light">
+                        <h2 class="fw-bold m-0">{{ $project->title }}</h2>
+                        @isset($project->desc)
+                            <div class="mb-0">{!! $project->desc !!}</div>
+                        @endisset
+                        @isset($project->location)
+                            <h4 class="fw-bold">{{ $project->location }}</h4>
+                        @endisset
+                        @isset($project->location)
+                            <p class="text-muted">{{ $project->date }}</p>
+                        @endisset
                     </div>
                 </div>
             </div>
@@ -88,12 +95,12 @@
 
             <div class="row">
                 <div class="col-12 py-5 p-sm-5">
-                    <h2>Gallery</h2>
+                    <h3 class="text-center mb-5">Gallery</h3>
                     <div class="row gy-3">
                         <?php $images = json_decode($project->images); ?>
                         @foreach ($images as $image)
                             <div class="col-6 col-sm-4 project-img-container">
-                                <img class="h-100 w-100 project-img" src="{{ Voyager::image($image) }}" />
+                                <img class="h-100 w-100 project-img" alt="Image gallery of {{ $project->title }}" src="{{ Voyager::image($image) }}" loading="lazy" />
                             </div>
                         @endforeach
                     </div>

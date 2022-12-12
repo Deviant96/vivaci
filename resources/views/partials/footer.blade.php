6345d39882359178c37492a11252dfd490b91051
Miretazam Ciptaprima <mcrealdeviant@gmail.com>
2022-12-12 20:08:17 +0700

    Fixed footer mobile layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,8 +8,8 @@
                 </div>
             </div>
         </div>
-        <div class="row mt-5">
-            <div class="col-12 col-sm-4 order-3 order-sm-1 mt-5 mt-sm-0">
+        <div class="row mt-3 mt-sm-5">
+            <div class="col-12 col-sm-4 order-3 order-sm-1 mt-3 mt-sm-0">
                 <h4 class="fw-bold">{{ setting('site.title') }}</h4>
                 <p class="text-muted">Copyrights © {{ date("Y") }}<br>
                     All Rights Reserved by <span class="text-uppercase">{{ setting('site.title') }}</span></p>
@@ -17,7 +17,7 @@
             <div class="col-12 col-sm-4 d-flex justify-content-sm-center order-1 order-sm-2">
                 @include('components.socials-footer')
             </div>
-            <div class="col-12 col-sm-4 d-sm-flex flex-column align-items-end order-2 order-sm-3 mt-3 mt-sm-0">
+            <div class="col-12 col-sm-4 d-sm-flex flex-column align-items-end order-2 order-sm-3 mt-3 mt-sm-0 d-none d-sm-block">
                 <div>
                     <a href="{{ route('contact') }}" class="btn btn-primary mt-2">Connect With Us</a>
                 </div>

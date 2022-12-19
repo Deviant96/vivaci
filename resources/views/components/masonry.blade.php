8792d228fd56850b5610a8bdcfccfe6546c3afe6
Miretazam Ciptaprima <mcrealdeviant@gmail.com>
2022-12-20 01:49:19 +0700

    Added imageLoaded library to prevent images overlapping

--- resources/views/components/masonry.blade.php
+++ resources/views/components/masonry.blade.php
@@ -1,4 +1,5 @@
 <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+<script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
 <script>
     var elem = document.querySelector('.grid');
     var msnry = new Masonry(elem, {
@@ -8,5 +9,11 @@
 </script>
 
 <script>
-    msnry.layout() // Initialize masonry layout to prevent images overlapping
+   
+//    msnry.imagesLoaded().progress(function() {
+    
+//     });
+    imagesLoaded('.grid', function() {
+        msnry.layout() // Initialize masonry layout to prevent images overlapping
+    });
 </script>
